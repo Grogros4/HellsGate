c10c6f1d01ce73ec05fd824ac6c8dce8d7ba25e8
YaroST12 <yaro330@gmail.com>
2018-03-31 12:08:30 +0100

    state_notifier: add a third case for boosting
    Added a third case for boosting things.
    For sake of boosting devfreq on fingerprint wakeup,
    since STATE_RESUME is getting called a when screen
    goes online, but sometimes boost is needed earlier.
    Signed-off-by: YaroST12 <yaro330@gmail.com>
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- include/linux/state_notifier.h
+++ include/linux/state_notifier.h
@@ -5,6 +5,7 @@
 
 #define STATE_NOTIFIER_ACTIVE		0x01
 #define STATE_NOTIFIER_SUSPEND		0x02
+#define STATE_NOTIFIER_BOOST		0x03
 
 struct state_event {
 	void *data;
@@ -14,6 +15,7 @@ extern bool state_suspended;
 extern bool use_fb_notifier;
 extern void state_suspend(void);
 extern void state_resume(void);
+extern void state_boost(void);
 int state_register_client(struct notifier_block *nb);
 int state_unregister_client(struct notifier_block *nb);
 int state_notifier_call_chain(unsigned long val, void *v);
